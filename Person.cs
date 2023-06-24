fe3101a53bba744bc35fe039d207c9d05851bfe8
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-06-24 14:27:20 +0100

    create Person class

---
+++ Person.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharp_fundamentals
+{
+	public class Person
+	{
+
+        public string FirstName;
+        public string LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is " + FirstName + " " + LastName);
+
+        }
+
+
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CSharp_fundamentals;
+
 Console.WriteLine("Hello, World!");
+
+
+var person = new Person();
+person.FirstName = "Doris";
+person.LastName = "Siu";
+person.Introduce();
