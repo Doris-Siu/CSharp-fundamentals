1e8b7dad0ffa699588172378fddee191a60e428b
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-06-24 16:18:44 +0100

    string Format, Join and verbatim string

--- Program.cs
+++ Program.cs
@@ -8,7 +8,26 @@ Console.WriteLine("Hello, World!");
 var person = new Person();
 person.FirstName = "Doris";
 person.LastName = "Siu";
+//string concatenation
 person.Introduce();
+
+// string format
+var myFullName = string.Format("My name is {0} {1}.", person.FirstName, person.LastName);
+Console.WriteLine(myFullName);
+
+//string - combine elements of an array
+var names = new string[3] { "Doris", "Janice", "Sabrina" };
+var formattedNames = string.Join(",", names);
+Console.WriteLine(formattedNames);
+
+
+//verbatim string
+var text = @"Hi Doris
+Look into the following paths
+c:\folder1\folder2
+c:\folder3\folder4";
+
+Console.WriteLine(text);
 
 var calculator = new Calculator();
 var result = calculator.Add(1, 2);
@@ -29,4 +48,3 @@ Console.WriteLine(flags[0]);
 Console.WriteLine(flags[1]);
 Console.WriteLine(flags[2]);
 
-var names = new string[3] { "Doris", "Janice", "Sabrina" }; 
