683e816b76d34475b0e5f6eaabfc7d99b23f9bea
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-06-24 15:12:16 +0100

    declare arrays, with object initialisation

--- Program.cs
+++ Program.cs
@@ -13,3 +13,20 @@ person.Introduce();
 var calculator = new Calculator();
 var result = calculator.Add(1, 2);
 Console.WriteLine(result);
+
+
+var numbers = new int[3];
+numbers[0] = 1;
+
+Console.WriteLine(numbers[0]);
+Console.WriteLine(numbers[1]);
+Console.WriteLine(numbers[2]);
+
+var flags = new bool[3];
+flags[0] = true;
+
+Console.WriteLine(flags[0]);
+Console.WriteLine(flags[1]);
+Console.WriteLine(flags[2]);
+
+var names = new string[3] { "Doris", "Janice", "Sabrina" }; 
