95f4083eadf5d30973254cada816edb45e23943b
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-06-26 14:37:37 +0100

    DateTime structure in System

--- Program.cs
+++ Program.cs
@@ -189,5 +189,20 @@ integers.Clear();
 Console.WriteLine("Count: " + integers.Count);
 
 
+// DateTime structure in System
 
+//var dateTime = new DateTime(2021, 1, 1);
+var now = DateTime.Now;
+var today = DateTime.Today;
 
+Console.WriteLine("Hour: " + now.Hour);
+Console.WriteLine("Minute: " + now.Minute);
+
+var tomorrow = now.AddDays(1);
+var yesterday = now.AddDays(-1);
+
+Console.WriteLine(now.ToLongDateString());
+Console.WriteLine(now.ToShortDateString());
+Console.WriteLine(now.ToLongTimeString());
+Console.WriteLine(now.ToShortTimeString());
+Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));
