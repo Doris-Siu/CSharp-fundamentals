78f229a4ff6235279906c741767b6d76f4653548
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-06-29 16:09:49 +0100

    ProceduralProgramming
    turn the Strings methods into a number of function with return type and take argument

---
+++ ProceduralProgramming.cs
@@ -0,0 +1,97 @@
+﻿using System;
+namespace CSharp_fundamentals
+{
+	public class ProceduralProgramming
+	{
+        
+        public bool IsConsecutiveList(List<int> list)
+        {
+            // copied the list to a new array because I didn't want to call Sort()
+            // impact the argument of this method. this method only expects a true/false answer 
+            // as a result. So, this method should not have a side-effect.
+            var numbers = new int[list.Count];
+            list.CopyTo(numbers);
+            Array.Sort(numbers);
+
+            for (var i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] != numbers[i - 1] + 1)
+                    return false;
+            }
+
+            return true;
+        }
+
+        
+        public bool ContainsDuplicates(List<int> numbers)
+        {
+            var uniques = new List<int>();
+            foreach (var number in numbers)
+            {
+                if (!uniques.Contains(number))
+                    uniques.Add(number);
+                else
+                    return true;
+            }
+
+            return false;
+        }
+
+        
+        public bool IsValidTime(string time)
+        {
+            if (String.IsNullOrWhiteSpace(time))
+                return false;
+
+            var components = time.Split(':');
+            if (components.Length != 2)
+                return false;
+
+            try
+            {
+                var hour = Convert.ToInt32(components[0]);
+                var minute = Convert.ToInt32(components[1]);
+
+                return (hour >= 0 && hour <= 23 && minute >= 0 && minute <= 59);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        
+        public string GetVariableName(string input)
+        {
+            if (String.IsNullOrWhiteSpace(input))
+                return "";
+
+            var variableName = "";
+            foreach (var word in input.Split(' '))
+            {
+                var wordWithPascalCase = char.ToUpper(word[0]) + word.ToLower().Substring(1);
+                variableName += wordWithPascalCase;
+            }
+
+            return variableName;
+        }
+
+
+        
+        public int CountVowels(string word)
+        {
+            var vowels = new List<char>(new char[] { 'a', 'e', 'o', 'u', 'i' });
+            var vowelsCount = 0;
+
+            foreach (var character in word.ToLower())
+            {
+                if (vowels.Contains(character))
+                    vowelsCount++;
+            }
+
+            return vowelsCount;
+        }
+    }
+}
+
+
